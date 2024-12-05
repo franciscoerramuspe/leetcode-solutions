78933c3c28ab883aac5b4b157a23c981db9672fe
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2024-12-05 11:22:05 -0500

    Update intersectionOfTwoLinkedLists.py

--- problems/linkedLists/intersectionOfTwoLinkedLists.py
+++ problems/linkedLists/intersectionOfTwoLinkedLists.py
@@ -43,3 +43,46 @@ def intersectionOfTwoLinkedLists(headA, headB):
             visited.add(curB)
             curB=curB.next
         return None
+
+
+# intento 2: hacerlo con O(1) memoria. Hint: primero pensar como hacer en listas de igual longitud,  luego pensar sihay alguna manera de reducir el problema a listas de igual longitud?
+# listas de igual longitud: atravesarlas a la misma vez hasta que el nodo de ambas listas sea el mismo
+# si guardamos la longitud de ambas listas, podemos atravesar la lista mas larga hasta el punto que su longitud sea igual a la lista mas corta
+# una vez tenemos ambas longitudes, podemos procesar ambas listas al mismo tiempo
+# 1 -> 2 -> 3 -> 4
+# 0 ->      3 -> 4 
+def intersectionOfTwoLinkedLists(headA, headB):
+    if not headA or not headB:
+        return None
+    lengthA, lengthB = 0
+    curA, curB = headA, headB
+    
+    while curA:
+        lengthA+=1
+        curA=curA.next
+    
+    while curB:
+        lengthB+=1
+        curB=curB.next
+    
+    # hay que determinar cual es la longitud mas grande (si es que una es mas grande), y atravesar hasta que sean iguales
+    largest= max(lengthA, lengthB)
+    smallest= min(lengthA, lengthB)
+    diff = largest-smallest
+    listUsed= headA if largest == lengthA else headB
+    l1 = listUsed
+
+    while diff > 0:
+        l1=l2.next
+        diff-=1
+    
+    # ambas listas tienen la misma longitud
+    l2 = headB if listUsed == headA else headA
+
+    while l1 and l2:
+        if l1 == l2:
+            return l1
+        l1=l1.next
+        l2=l2.next
+    return None
+    
