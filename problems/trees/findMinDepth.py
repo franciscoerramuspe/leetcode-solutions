84cbe1d54c6382824afabd1bc7329f9defb85e81
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2024-12-09 14:34:24 -0500

    added min depth

--- problems/trees/findMinDepth.py
+++ problems/trees/findMinDepth.py
@@ -4,6 +4,9 @@ Given a binary tree, find its minimum depth.
 The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
 
 Note: A leaf is a node with no children.
+
+Time: O(N)
+Space: O(N)
 '''
 def findMinDepth(root):
     if not root:
