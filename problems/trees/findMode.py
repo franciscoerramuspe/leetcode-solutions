1bfb92b73910c6e50b7d1eff591dea59d6ba8678
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2024-12-11 10:14:00 -0500

    added tests

--- problems/trees/findMode.py
+++ problems/trees/findMode.py
@@ -1,3 +1,18 @@
+'''
+Given the root of a binary search tree (BST) with duplicates, return all the mode(s) (i.e., the most frequently occurred element) in it.
+
+If the tree has more than one mode, return them in any order.
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Time: O(N)
+Space: O(N)
+'''
+
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
@@ -24,3 +39,10 @@ class Solution:
         ans=[key for key, val in counter.items() if val == maxVal]
                 
         return ans
+
+
+        # tests
+        # input: [4, 3, 5, 1, null, 6], ans= [4, 3, 5, 1, 6]
+        # input: [4, 2, 7, 1, 2, 7, 9, null, null, 9], ans= [7, 9]
+        # input: [6, 3, 8, 3, 4], ans= [3]
+        #input:  [1], ans=[1]
