7a0048a43583b2e1adb1ffc199780c7f052216a4
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2024-12-12 21:36:39 -0500

    added time and space

--- problems/trees/recoverBST.py
+++ problems/trees/recoverBST.py
@@ -2,6 +2,9 @@
 You are given the root of a binary search tree (BST),
 
 where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+Time: O(2 * n) -> O(n)
+Space: O(n)
 '''
 
 
